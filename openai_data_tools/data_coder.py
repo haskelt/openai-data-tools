c7dc52a3a7f7fae3523d03539ee9706520784259
Todd Haskell <todd@craggypeak.com>
2024-01-05 06:27:14 -0800

    Simplified code for DataProcessor and DataCoder

--- openai_data_tools/data_coder.py
+++ openai_data_tools/data_coder.py
@@ -14,7 +14,7 @@ class DataCoder (DataProcessor):
         # In addition to whatever criteria are provided in <instructions>, we ask for the 
         # response back in JSON format so that we can easily identify the yes/no answer
         # for purposes of scoring the response. 
-        self.instruction_messages=[
+        self._instruction_messages=[
             {"role": "system", "content": instructions + 
              """ Respond in JSON format with the key 'answer' for your yes/no response, and 
              the key 'explanation' for an explanation of the response."""}
@@ -25,25 +25,25 @@ class DataCoder (DataProcessor):
         # provided to the model after the instructions and before the item to be classified.
         # Should be in the format a list of dicts, where each dict has keys 'item', 'target',
         # and 'explanation'. The 'target' value should contain a 0 or 1.
-        self.example_messages = []
+        self._example_messages = []
         if examples:
             for example in examples:
-                self.example_messages.append({'role': 'user', 'content': example['item'] })
-                self.example_messages.append({'role': 'assistant', 'content': json.dumps({
+                self._example_messages.append({'role': 'user', 'content': example['item'] })
+                self._example_messages.append({'role': 'assistant', 'content': json.dumps({
                         'answer': 'yes' if example['target']=='1' else 'no', 
                         'explanation': example['explanation']}
                 )})
 
     # Initializes a data structure for storing output data for each item
     def _init_output(self, n_items):
-        self._data['output'] = [None] * n_items
-        self._data['explanation'] = [None] * n_items
+        self._output = [None] * n_items
+        self._explanations = [None] * n_items
     
     # Takes <response> and stores it as part of the output data for item <i>
     def _store_output(self, i, response):
         output = json.loads(response['choices'][0]['message']['content'])
-        self._data['output'][i] = (np.array(output['answer'].lower().rstrip('.')) == 'yes').astype(dtype=int).tolist()
-        self._data['explanation'][i] = output['explanation']
+        self._output[i] = (np.array(output['answer'].lower().rstrip('.')) == 'yes').astype(dtype=int).tolist()
+        self._explanations[i] = output['explanation']
 
     def explanations(self):
-        return self._data['explanation']
+        return self._explanations
--- openai_data_tools/data_processor.py
+++ openai_data_tools/data_processor.py
@@ -10,17 +10,16 @@ from . import openai_functions as ai
 class DataProcessor:
     
     def __init__(self, model, instructions, examples=None, api_key=None, timeout=30):
-        self._data = {}
-        self.model = model
-        self.api_key = api_key
+        self._model = model
+        self._api_key = api_key
         # <timeout> controls how long we wait (in seconds) for a  response from OpenAI
         # before giving up and trying again.
-        self.timeout = timeout
+        self._timeout = timeout
         self._configure_instructions(instructions)
         self._configure_examples(examples)
 
     def _configure_instructions(self, instructions):
-        self.instruction_messages=[
+        self._instruction_messages=[
             {"role": "system", "content": instructions }
         ]
 
@@ -30,37 +29,37 @@ class DataProcessor:
         # classified.
         # <examples> should be in the format a list of dicts, where each dict has keys
         # 'item' and 'target', where target is the desired response.
-        self.example_messages = []
+        self._example_messages = []
         if examples:
             for example in examples:
-                self.example_messages.append({'role': 'user', 'content': example['item']})
-                self.example_messages.append({'role': 'assistant', 'content': example['target']})
+                self._example_messages.append({'role': 'user', 'content': example['item']})
+                self._example_messages.append({'role': 'assistant', 'content': example['target']})
     
     # Initializes a data structure for storing output data for each item
     def _init_output(self, n_items):
-        self._data['output'] = [None] * n_items
+        self._output = [None] * n_items
     
     # Takes <response> and stores it as part of the output data for item <i>
     def _store_output(self, i, response):
-        self._data['output'][i] = response['choices'][0]['message']['content']
+        self._output[i] = response['choices'][0]['message']['content']
 
     # Returns a list with the model output for each item based on the last call to <process>
     def _get_output(self):
-        return self._data['output']
+        return self._output
 
     # Processes a single item
     def _process_item(self, item):
-        if self.mode == 'live':
+        if self._mode == 'live':
             response = ai.make_request(
-                self.model, 
-                messages = self.instruction_messages 
-                   + self.example_messages 
+                self._model, 
+                messages = self._instruction_messages 
+                   + self._example_messages 
                    + [{'role': 'user', 'content': item}] 
             )
         # This provides a way to test that your script runs properly without actually
         # making a call to the OpenAI API. It returns the original item without changes
         # as output. To be more realistic, it also generates a small delay.
-        elif self.mode == 'simulated':
+        elif self._mode == 'simulated':
             time.sleep(.2)
             response = {'choices': [{'message': {'content': item}}], 'usage': {}}
 
@@ -84,20 +83,9 @@ class DataProcessor:
     # Also creates an attribute on the processor's _data object:
     # <item_data> - A list containing the responses from the model for each item
     def process(self, items, mode='live'):
-        ai.configure(api_key=self.api_key, timeout=self.timeout)
-        self.mode = mode
+        ai.configure(api_key=self._api_key, timeout=self._timeout)
+        self._mode = mode
         self._init_output(len(items))
         self._process_items(items)
         return self._get_output()
     
-    # Saves the _data attribute of the object to <filename>, so you can restore the
-    # state of the object in another session.
-    def dump(self, filename):
-        with open(filename, "wb") as outfile:
-            pickle.dump(self._data, outfile)
-
-    # Restores the _data attribute of the object that was previously dumped to 
-    # <filename>
-    def restore(self, filename):
-        with open(filename, "rb") as infile:
-            self._data = pickle.load(infile)
