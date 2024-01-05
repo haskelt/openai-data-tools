d7ed9a89b23bd82775f1eedcc2d6859a83ea7dc6
Todd Haskell <todd@craggypeak.com>
2024-01-05 11:46:32 -0800

    Converted MultiScorer into a new RunAggregator class

--- openai_data_tools/__init__.py
+++ openai_data_tools/__init__.py
@@ -1,6 +1,6 @@
 from .data_processor import DataProcessor
 from .data_coder import DataCoder
 from .scorer import Scorer
-from .multi_scorer import MultiScorer
 from .classification_scorer import ClassificationScorer
+from .run_aggregator import RunAggregator
 from .chat import Chat
