c7648705083f29d898088c0b92d30987cbfb5aa6
Todd Haskell <todd@craggypeak.com>
2024-01-05 06:16:46 -0800

    Cleaned up code for Scorer objects

--- openai_data_tools/classification_scorer.py
+++ openai_data_tools/classification_scorer.py
@@ -10,15 +10,15 @@ class ClassificationScorer (Scorer):
         at = np.array(targets)
         # to simplify calculations, we treat the 1 and 0 values in the arrays as booleans,
         # and use logical operations on them
-        self._data['n_truepos'] = np.logical_and(ar, at).sum()
-        self._data['n_trueneg'] = np.logical_and(np.logical_not(ar), np.logical_not(at)).sum()
-        self._data['n_falsepos'] = np.logical_and(ar, np.logical_not(at)).sum()
-        self._data['n_falseneg'] = np.logical_and(np.logical_not(ar), at).sum()
+        self._n_truepos = np.logical_and(ar, at).sum()
+        self._n_trueneg = np.logical_and(np.logical_not(ar), np.logical_not(at)).sum()
+        self._n_falsepos = np.logical_and(ar, np.logical_not(at)).sum()
+        self._n_falseneg = np.logical_and(np.logical_not(ar), at).sum()
 
     # Returns the proportion of all positive responses where that was the correct classification
     def precision(self):
-        return self._data['n_truepos'] / (self._data['n_truepos'] + self._data['n_falsepos'])
+        return self._n_truepos / (self._n_truepos + self._n_falsepos)
 
     # Returns the proportion of all items that should be classified as positive that were actually classified as positive
     def recall(self):
-        return self._data['n_truepos'] / (self._data['n_truepos'] + self._data['n_falseneg'])
+        return self._n_truepos / (self._n_truepos + self._n_falseneg)
--- openai_data_tools/scorer.py
+++ openai_data_tools/scorer.py
@@ -4,13 +4,12 @@ class Scorer:
 
     # Creates a Scorer object for comparing <output> with <targets>
     def __init__(self, output, targets):
-        self._data = {}
-        self._data['scoring'] = (np.array(output) == np.array(targets)).astype(dtype=int)
+        self._scoring = (np.array(output) == np.array(targets)).astype(dtype=int)
     
     # Returns a list with 1 for each element i where <output[i]> matches <targets[i]> and 0 for each element where they don't match
     def scores(self):
-        return self._data['scoring'].tolist()
+        return self._scoring.tolist()
 
     # Returns the proportion of items where <output[i]> matches <targets[i]>
     def accuracy(self):
-        return np.mean(self._data['scoring'])
+        return np.mean(self._scoring)
