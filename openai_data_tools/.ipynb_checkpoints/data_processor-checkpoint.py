321e0b6d0761c64366b63e3fd408c859981c8b0a
Todd Haskell <haskelltr@gmail.com>
2025-05-24 07:35:11 -0700

    Added a verbose parameter to allow turning off the progress indicator

--- openai_data_tools/.ipynb_checkpoints/data_processor-checkpoint.py
+++ openai_data_tools/.ipynb_checkpoints/data_processor-checkpoint.py
@@ -65,19 +65,18 @@ class DataProcessor:
         for i, item in enumerate(items):
             response = self._process_item(item)
             self._store_output(i, response)
-            print('Progress: {:.0%}'.format((i+1)/n_items), end='\r')
-        print('')
+            if self._verbose: print('Progress: {:.0%}'.format((i+1)/n_items), end='\r')
+        if self._verbose: print('')
     
     # Asks the model to go through each item in the list <items>, apply the processing
     # specified in the instructions, and return the result.  <mode> can be 'live' or
     # 'simulated', and controls whether we send requests to OpenAI and get a real
     # response, or just provide a simulated response (this can be useful for testing
-    # purposes).
-    # 
-    # Also creates an attribute on the processor's _data object:
-    # <item_data> - A list containing the responses from the model for each item
-    def process(self, items, mode='live'):
+    # purposes). <verbose> controls whether we display a progress indicator after 
+    # processing each item.
+    def process(self, items, mode='live', verbose=True):
         self._mode = mode
+        self._verbose = verbose
         self._init_output(len(items))
         self._process_items(items)
         return self._get_output()
